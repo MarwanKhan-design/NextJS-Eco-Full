68546a82ea5589f2f7ca48ffee4222a8314884a1
Marwan <marwanpsh@gmail.com>
2025-08-27 19:34:48 +0500

    fixed single product props

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,7 +1,11 @@
 import ProductDetail from "@/app/components/ProductDetail"
 import { stripe } from "@/lib/stripe"
 
-export default async function ProductPage({ params }: { params: { id: string } }) {
+interface ProductPageProps {
+    params: { id: string }
+}
+
+export default async function ProductPage({ params }: ProductPageProps) {
     const product = await stripe.products.retrieve(params.id, {
         expand: ["default_price"],
     })
